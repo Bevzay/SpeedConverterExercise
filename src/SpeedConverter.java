07672ab29c38b0f1d9bcb70cf4cc805a1c4ba191
Beverly Ann Gupit <Bevzay@users.noreply.github.com>
2020-06-07 14:12:56 +1200

    Initial commit and Part 1 of exercise

---
+++ src/SpeedConverter.java
@@ -0,0 +1,17 @@
+public class SpeedConverter {
+
+    /*
+    * Part 1
+    *
+    * Write a method called toMilesPerHour that has 1 parameter of type double with the name kilometersPerHour.
+    * This method needs to return the rounded value of the calculation of type long.
+    *
+    * If the parameter kilometersPerHour is less than 0,
+    * the method toMilesPerHour needs to return -1 to indicate invalid value
+    *
+    * If it is positive, calculate the value of miles per hour, round it and return it.
+     */
+
+    
+
+}
