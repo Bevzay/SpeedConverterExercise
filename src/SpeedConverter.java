a597240bd1e064b3cc6c3e3201ac529cfcef9025
Beverly Ann Gupit <Bevzay@users.noreply.github.com>
2020-06-07 14:28:55 +1200

    Part 2 exercise

--- src/SpeedConverter.java
+++ src/SpeedConverter.java
@@ -22,4 +22,20 @@ public class SpeedConverter {
         }
     }
 
+    /*
+    * Part 2
+    *
+    * Write another method called printConversion with 1 parameter of type double with the name kilometersPerHour
+    * This method should not return anything (void)
+    * and it needs to calculate milesPerHour from the kilometersPerHour parameter.
+    * Then it needs to print a message in the format "XX km/h = YY mi/h"
+    *
+    * XX represents kilometresPerHour
+    * YY represents the rounded milesPerHour from the kilometresPerHour parameter
+    *
+    * If the parameter kilometresPerHour is < 0 then print the text "Invalid Value"
+     */
+
+
+
 }
